26cd5fd2d574fbb696e42424a60bcdce052772a1
ClemDSC <clemence.p14@gmail.com>
2022-10-21 10:50:14 +0200

    add filter by user name

--- src/app/components/user-list/user-list.component.ts
+++ src/app/components/user-list/user-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { UserHttpService } from 'src/app/shared/user-http.service';
+import { User } from 'src/models/user.model';
 
 @Component({
   selector: 'app-user-list',
@@ -8,9 +9,18 @@ import { UserHttpService } from 'src/app/shared/user-http.service';
 })
 export class UserListComponent implements OnInit {
 
+  @Input()
+  searchedContentChild: string = "";
+
   userList: any[] = [];
 
+  userListFiltered: User[] = [];
+
   constructor(private userApi: UserHttpService) { }
+
+  ngOnChanges(): void {
+    this.userListFiltered = this.userList.filter(user => user.name.toLowerCase().includes(this.searchedContentChild.toLowerCase()))
+  }
 
   ngOnInit(): void {
     this.userApi.getUserList().subscribe(data => {
