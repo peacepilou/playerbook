955c4b04fdf5ef1ba79200027e0706a891ad7321
ClemDSC <clemence.p14@gmail.com>
2022-10-22 15:39:56 +0200

    delete useless code

--- src/app/components/checkbox-filter-users/checkbox-filter-users.component.ts
+++ src/app/components/checkbox-filter-users/checkbox-filter-users.component.ts
@@ -1,16 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { UserHttpService } from 'src/app/shared/user-http.service';
-import { User } from 'src/models/user.model';
 
 @Component({
   selector: 'app-checkbox-filter-users',
   templateUrl: './checkbox-filter-users.component.html',
-  styleUrls: ['./checkbox-filter-users.component.scss']
+  styleUrls: ['./checkbox-filter-users.component.scss'],
 })
 export class CheckboxFilterUsersComponent implements OnInit {
-
-  userList: User[] = [];
-
   isPro: boolean = false;
   isCasual: boolean = false;
   isLeader: boolean = false;
@@ -20,11 +15,7 @@ export class CheckboxFilterUsersComponent implements OnInit {
   isProactive: boolean = false;
   isExtravert: boolean = false;
 
-  constructor(private userApi: UserHttpService) { }
+  constructor() {}
 
-  ngOnInit(): void {
-    this.userApi.getUserList().subscribe((data) => {
-      this.userList = data;
-    });
-  }
+  ngOnInit(): void {}
 }
