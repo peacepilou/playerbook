25bcb13be11f7b62f3bb4dc225d220098da12410
ClemDSC <clemence.p14@gmail.com>
2022-10-23 19:30:02 +0200

    keep and can listen checkbox values in searchList component

--- src/app/components/search-list/search-list.component.ts
+++ src/app/components/search-list/search-list.component.ts
@@ -8,11 +8,45 @@ import { Component, OnInit } from '@angular/core';
 export class SearchListComponent implements OnInit {
   searchedContent: string = '';
 
+  checkBoxValue: {
+    isPro: boolean;
+    isCasual: boolean;
+    isLeader: boolean;
+    isPvpFriendly: boolean;
+    isPveFriendly: boolean;
+    isNocturnal: boolean;
+    isProactive: boolean;
+    isExtravert: boolean;
+  } = {
+    isPro: false,
+    isCasual: false,
+    isLeader: false,
+    isPvpFriendly: false,
+    isPveFriendly: false,
+    isNocturnal: false,
+    isProactive: false,
+    isExtravert: false,
+  };
+
   constructor() {}
 
   ngOnInit(): void {}
 
   catchSearchedContent(event: string): void {
     this.searchedContent = event;
+  }
+
+  catchValuesCheckbox(event: {
+    isPro: boolean;
+    isCasual: boolean;
+    isLeader: boolean;
+    isPvpFriendly: boolean;
+    isPveFriendly: boolean;
+    isNocturnal: boolean;
+    isProactive: boolean;
+    isExtravert: boolean;
+  }): void {
+    this.checkBoxValue = event;
+    console.log("valeur dans le parent", event)
   }
 }
