7f9e49ebcd0a739971975a92449f44614be6811e
mickaël blanchet <mickael.blanchet5555@gmail.com>
2022-10-27 17:03:19 +0200

    added a back to home button to forms, forced the position of button next

--- src/app/components/form/global-form/global-form.component.ts
+++ src/app/components/form/global-form/global-form.component.ts
@@ -37,7 +37,7 @@ export class GlobalFormComponent implements OnInit {
     private router : Router
   ) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void {    
   }
 
   receiveFirstForm(event : User) : void {
