fc39dc81ee4d93d30f004f03142a2c1ba2e9097c
Blanchet Mickaël <mickael.blanchet5555@gmail.com>
2022-11-24 14:32:03 +0100

    Update connexion-form.component.ts
    removed a console.log

--- src/app/components/connexion-form/connexion-form.component.ts
+++ src/app/components/connexion-form/connexion-form.component.ts
@@ -27,10 +27,7 @@ export class ConnexionFormComponent implements OnInit {
       localStorage.clear();
       this.httpRoutes.navigateByUrl('/home');
       this.jwtToken = jwt.access_token;
-      localStorage.setItem("tokenId", jwt.access_token); 
-      
-      console.log("jwtToken", this.jwtToken);
-           
+      localStorage.setItem("tokenId", jwt.access_token);           
     })
   }
 }
