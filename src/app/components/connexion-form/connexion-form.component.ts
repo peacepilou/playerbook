7945ec18bcb1bb310fb01081c3a3a6c22709c97e
ClemDSC <clemence.p14@gmail.com>
2022-11-24 19:58:03 +0100

    install pack to show toast on some events / use it when login and logout

--- src/app/components/connexion-form/connexion-form.component.ts
+++ src/app/components/connexion-form/connexion-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/shared/auth.service';
+import { HotToastService } from '@ngneat/hot-toast';
 
 @Component({
   selector: 'app-connexion-form',
@@ -16,7 +17,8 @@ export class ConnexionFormComponent implements OnInit {
 
   constructor(
     private authS : AuthService,
-    private httpRoutes : Router) { }
+    private httpRoutes : Router,
+    private toast: HotToastService) { }
 
   ngOnInit(): void {
   }
@@ -26,8 +28,20 @@ export class ConnexionFormComponent implements OnInit {
     .subscribe((jwt) => {
       localStorage.clear();
       this.httpRoutes.navigateByUrl('/home');
+      this.showToast();
       this.jwtToken = jwt.access_token;
       localStorage.setItem("tokenId", jwt.access_token);           
     })
   }
+
+  showToast() {
+    this.toast.success('Connexion réussie !', {
+      position: 'bottom-right',
+      dismissible: true,
+      style: {
+        border: '1px solid #8738BB'
+      }
+    });
+  }
+
 }
